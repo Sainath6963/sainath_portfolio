6e873b06270dd11187d2f95d726f3a13f8469807
Sainath6963 <103501531+Sainath6963@users.noreply.github.com>
2024-01-17 10:36:08 +0530

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'java development ' ?
+            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>java </ToggleButton>
+            :
+            <ToggleButton value="web app" onClick={() => setToggle('web app')}>java </ToggleButton>
+          }
+          <Divider />
+          {toggle === 'web development' ?
+            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>web development</ToggleButton>
+            :
+            <ToggleButton value="android app" onClick={() => setToggle('android app')}>web development</ToggleButton>
+          }
+         
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
